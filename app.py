d234692bdbf4b15d2d70ebd920db9b2be5ea5387
akash@maticui <akash@maticui.com>
2025-09-08 17:37:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+def dumnmy():
+    print(__name__)
+
+
+dumnmy()
---
+++ index.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "home route"
+
+
+@app.route("/test", methods=["POST"])
+def about():
+    data = request.json
+    print(data)
+    return "about route"
+
+
+@app.route("/update", methods=["PUT"])
+def updateNote():
+    return "Note Update Success"
+
+
+@app.route("/remove", methods=["DELETE"])
+def removeNote():
+    return "Note remove Success"
+
+
+# server on
+app.run(debug=True)
